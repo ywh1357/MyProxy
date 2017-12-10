084e3ed4e1a4d684e4d23ff53128962824014bfc
ywh1357 <ywh1357@gmail.com>
2017-12-10 14:11:29 +0800

    ...

--- Common/iohelper.h
+++ Common/iohelper.h
@@ -28,13 +28,13 @@ namespace MyProxy {
 	struct TnS {
 		typedef T type;
 		const size_t size;
-		constexpr TnS(size_t size = sizeof(T)):size(size){}
+		constexpr TnS(size_t size = sizeof(T)) :size(size) {}
 	};
 
-	const TnS<uint8_t> _1B;
-	const TnS<uint16_t> _2B;
-	const TnS<uint32_t> _4B;
-	const TnS<uint64_t> _8B;
+	constexpr TnS<uint8_t> _1B;
+	constexpr TnS<uint16_t> _2B;
+	constexpr TnS<uint32_t> _4B;
+	constexpr TnS<uint64_t> _8B;
 	using _datavec = TnS<DataVec>;
 
 	template <typename TupleType, typename Func, size_t ...I>
@@ -86,88 +86,19 @@ namespace MyProxy {
 		IoHelper(DataVec &vec, bool _convert = false) :ios(makeBuffer(vec)), convert(_convert) {}
 
 		template <typename T>
-		void write(const T *value, size_t count) {
-			ios.write(reinterpret_cast<const char*>(value), count);
-		}
-		template <typename T>
-		void write(const T& value) {
-			if (convert && std::is_integral<T>::value) {
-				T temp = value;
-				temp = boost::endian::native_to_big(temp);
-				ios.write(reinterpret_cast<const char*>(&temp), sizeof(T));
-			}
-			else {
-				ios.write(reinterpret_cast<const char*>(&value), sizeof(T));
-			}
-		}
-		template <typename T>
-		void write(const std::vector<T>& v) {
-			for (size_t i = 0; i < v.size(); i++) {
-				write(v[i]);
-			}
-		}
-		template <>
-		void write<char>(const DataVec& v) {
-			ios.write(v.data(), v.size());
-		}
+		void write(const T& value, size_t bytes = sizeof(T));
 
 		template <typename T>
-		void read(T& value) {
-			ios.read(reinterpret_cast<char*>(&value), sizeof(T));
-			if (convert && std::is_integral<T>::value) {
-				boost::endian::big_to_native_inplace(value);
-			}
-		}
-		template <typename T>
-		void read(T* value, size_t count) {
-			ios.read(reinterpret_cast<char*>(value), count);
-		}
-		template <typename T>
-		void read(std::vector<T>& v) {
-			for (size_t i = 0; i < v.size(); i++) {
-				read(v[i]);
-			}
-		}
-		template <>
-		void read<char>(DataVec& v) {
-			ios.read(v.data(),v.size());
-		}
-		void read(DataVec& v, size_t count) {
-			v.resize(count);
-			ios.read(v.data(), count);
-		}
+		void read(T& value, size_t bytes = sizeof(T));
 
 		template <typename T>
-		T getValue(size_t count = sizeof(T)) {
-			T value;
-			read(value);
-			return value;
-		}
-
-		template <>
-		DataVec getValue(size_t count) {
-			DataVec value;
-			read(value, count);
-			return value;
-		}
+		T getValue(size_t count = sizeof(T));
 
 		template <typename ...Args>
-		void getValues(Args& ...args) {
-			std::initializer_list<int>{
-				([this](auto &value) {
-					read(value);
-				}(args), 0)...
-			};
-		}
+		void getValues(Args& ...args);
 
 		template <typename ...Types, typename ...Args>
-		void getCastedValues(Args& ...args) {
-			std::initializer_list<int>{
-				([this](auto& value) {
-					getSomeValue<Types>(value);
-				}(args),0)...
-			};
-		}
+		void getCastedValues(Args& ...args);
 
 		template <typename ...OutputTypes, typename ...TnSs>
 		std::tuple<OutputTypes...> getTuple(TnSs ...tnss) {
@@ -175,59 +106,135 @@ namespace MyProxy {
 		}
 
 		template <typename ...Args>
-		void putValues(const Args& ...args) {
-			std::initializer_list<int>{
-				([this](const auto &value) {
-					write(value);
-				}(args), 0)...
-			};
-		}
+		void putValues(const Args& ...args);
 
 		template <typename ...Types, typename ...Args>
-		void putCastedValues(const Args& ...args) {
-			std::initializer_list<int>{
-				([this](const auto &value) {
-					write(static_cast<Types>(value));
-				}(args), 0)...
-			};
-		}
+		void putCastedValues(const Args& ...args);
+
 	private:
-		template <typename O,typename T>
+		template <typename O, typename T>
 		typename std::enable_if<!std::is_same<O, T>::value, void>::type
-		getSomeValue(T& value) {
+			getSomeValue(T& value) {
 			O temp;
 			read(temp);
 			value = static_cast<T>(temp);
 		}
 		template <typename O, typename T>
 		typename std::enable_if<std::is_same<O, T>::value, void>::type
-		getSomeValue(T& value) {
+			getSomeValue(T& value) {
 			read(value);
 		}
 		template <typename T>
 		static T parseType(size_t size) {
 			return T();
 		}
-		template <typename TNS>
-		typename TNS::type getSomeValue(TNS tns) {
-			typename TNS::type value = parseType<TNS::type>(tns.size);
-			read(value);
-			return value;
-		}
 		template <>
 		static DataVec parseType<DataVec>(size_t size) {
 			return DataVec(size);
 		}
-
+		template <typename T>
+		typename TnS<T>::type getSomeValue(TnS<T> tns) {
+			typename TnS<T>::type value = parseType<typename TnS<T>::type>(tns.size);
+			read(value);
+			return value;
+		}
 		DataVecBuf* makeBuffer(DataVec &vec) {
 			m_buf = std::make_shared<DataVecBuf>(vec);
 			return m_buf.get();
 		}
-
 		std::shared_ptr<DataVecBuf> m_buf;
 		std::iostream ios;
 		bool convert;
 	};
+
+	template<typename T>
+	inline void IoHelper::write(const T & value, size_t bytes)
+	{
+		if (convert && std::is_integral<T>::value) {
+			T temp = value;
+			temp = boost::endian::native_to_big(temp);
+			ios.write(reinterpret_cast<const char*>(&temp), sizeof(T));
+		}
+		else {
+			ios.write(reinterpret_cast<const char*>(&value), sizeof(T));
+		}
+	}
+
+	template<>
+	inline void IoHelper::write<std::vector<char>>(const std::vector<char> & value, size_t)
+	{
+		ios.write(value.data(), value.size());
+	}
+
+	template<typename T>
+	inline void IoHelper::read(T & value, size_t bytes)
+	{
+		ios.read(reinterpret_cast<char*>(&value), bytes);
+		if (convert && std::is_integral<T>::value) {
+			boost::endian::big_to_native_inplace(value);
+		}
+	}
+
+	template<>
+	inline void IoHelper::read(std::vector<char> & value, size_t bytes)
+	{
+		ios.read(value.data(), value.size());
+	}
+
+	template<typename T>
+	inline T IoHelper::getValue(size_t count)
+	{
+		T value;
+		read(value);
+		return value;
+	}
+
+	template <>
+	inline std::vector<char> IoHelper::getValue<std::vector<char>>(size_t count) {
+		std::vector<char> value(count);
+		read(value);
+		return value;
+	}
+
+	template<typename ...Args>
+	inline void IoHelper::getValues(Args & ...args)
+	{
+		std::initializer_list<int>{
+			([this](auto &value) {
+				read(value);
+			}(args), 0)...
+		};
+	}
+
+	template<typename ...Types, typename ...Args>
+	inline void IoHelper::getCastedValues(Args & ...args)
+	{
+		std::initializer_list<int>{
+			([this](auto& value) {
+				getSomeValue<Types>(value);
+			}(args), 0)...
+		};
+	}
+
+	template<typename ...Args>
+	inline void IoHelper::putValues(const Args & ...args)
+	{
+		std::initializer_list<int>{
+			([this](const auto &value) {
+				write(value);
+			}(args), 0)...
+		};
+	}
+
+	template<typename ...Types, typename ...Args>
+	inline void IoHelper::putCastedValues(const Args & ...args)
+	{
+		std::initializer_list<int>{
+			([this](const auto &value) {
+				write(static_cast<Types>(value));
+			}(args), 0)...
+		};
+	}
 
 	template<typename ...Args>
 	IoHelper & operator<<(IoHelper & os, const std::tuple<Args...>& tuple)
